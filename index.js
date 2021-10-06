23d1fd74ffca74a69243278cf3221722a37932c8
Пользователь <uchqunjonturdiev@gmail.com  git config --global user.name uchqun007>
2021-10-06 16:18:55 +0500

    js slider mini proect

--- index.js
+++ index.js
@@ -9,6 +9,23 @@
      container = document.querySelector('.container');
 
      line1.addEventListener('click', function () {
-         slider.style.display = 'none';
-     })
- })
+         slider.style.transform = 'translateX(0)';
+         active.style.top = '0';
+         container.style.background = '#c4d719';
+     });
+     line2.addEventListener('click', function () {
+        slider.style.transform = 'translateX(-25%)';
+        active.style.top = '80px';
+        container.style.background = '#195bd7';
+    });
+    line3.addEventListener('click', function () {
+        slider.style.transform = 'translateX(-50%)';
+        active.style.top = '160px';
+        container.style.background = '#19d762';
+    });
+    line4.addEventListener('click', function () {
+        slider.style.transform = 'translateX(-75%)';
+        active.style.top = '240px';
+        container.style.background = 'crimson';
+    });
+ });
